00655d6dfa7599b4fdc94c352702706ec515f855
jérémy_laurent <jeremy.laurentj@gmail.com>
2022-01-31 08:43:57 +0100

    modif

--- src/index.php
+++ src/index.php
@@ -2,5 +2,6 @@
 echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 echo '<p>update : 23 janvier 2022 </p>';
+echo 'jérémy';
 ?>
 
